448599cdc6894b241e92748128dd6edaadd3d28c
koh-yoshimoto <yoshimoto.koshiro@gmail.com>
2023-11-14 23:33:02 +0900

    compact player_score

--- webapp/sql/tenant/10_schema.sql
+++ webapp/sql/tenant/10_schema.sql
@@ -23,15 +23,12 @@ CREATE TABLE player (
 );
 
 CREATE TABLE player_score (
-  id VARCHAR(255) NOT NULL PRIMARY KEY,
   tenant_id BIGINT NOT NULL,
   player_id VARCHAR(255) NOT NULL,
   competition_id VARCHAR(255) NOT NULL,
   score BIGINT NOT NULL,
-  row_num BIGINT NOT NULL,
   created_at BIGINT NOT NULL,
-  updated_at BIGINT NOT NULL
+  updated_at BIGINT NOT NULL,
+  PRIMARY KEY (tenant_id, player_id, competition_id)
 );
 
-create index player_score_idx_1 on player_score(tenant_id, competition_id, player_id, row_num DESC);
-create index player_score_idx_2 on player_score(tenant_id, competition_id, row_num DESC);
